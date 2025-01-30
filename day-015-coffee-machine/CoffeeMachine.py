ef66cedba8281480d6daf07aeb351eca47a81959
jmr-enola <jmr.enola@gmail.com>
2025-01-31 02:17:59 +0800

    day 15

--- day-015-coffee-machine/CoffeeMachine.py
+++ day-015-coffee-machine/CoffeeMachine.py
@@ -1,3 +1,36 @@
-'''
-Flavors: espresso/latte/cappuccino
-'''
+resources = [
+    {"resource": "Water", "val": 100, "unit": "ml", "unit_prefix": False},
+    {"resource": "Milk", "val": 50, "unit": "ml", "unit_prefix": False},
+    {"resource": "Water", "val": 76, "unit": "g", "unit_prefix": False},
+    {"resource": "Water", "val": 2.5, "unit": "$", "unit_prefix": True},
+]
+
+def startMachine():
+    while (True): 
+        
+        choice = input("What would you like? (espresso/latte/cappuccino): ").lower()
+
+        if choice == "off":
+            print("Turning machine off...")
+            exit()
+
+        if choice == "report":
+            for resource in resources:
+                if resource["unit_prefix"]:
+                    print(f'{resource["resource"]}: {resource["unit"]}{resource["val"]}')
+                else:
+                    print(f'{resource["resource"]}: {resource["val"]}{resource["unit"]}')
+            continue
+
+        if choice == "espresso":
+            pass
+
+        if choice == "latte":
+            pass
+
+        if choice == "cappuccino":
+            pass
+
+
+startMachine()
+
