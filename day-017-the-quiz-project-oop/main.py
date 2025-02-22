bc5653085ea92ed5377b77ca6fb951ddb457925d
jmr-enola <jmr.enola@gmail.com>
2025-02-22 23:19:22 +0800

    day 17

--- day-017-the-quiz-project-oop/main.py
+++ day-017-the-quiz-project-oop/main.py
@@ -1,3 +1,9 @@
-'''
+from rsc.Data import question_data
+from rsc.QuestionModel import Question
 
-'''
+questions = []
+
+for data in question_data:
+    questions.append(Question(**data))
+
+questions[0].printData() 
